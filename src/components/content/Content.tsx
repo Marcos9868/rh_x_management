f4b18bbe53434af4214100b7038a94087bde67d6
Marcos9868 <marcos_m_ferreira@outlook.com>
2023-04-24 20:38:34 -0300

    feat(Add): home content component

---
+++ src/components/content/Content.tsx
@@ -0,0 +1,23 @@
+const Content = () => {
+  return (
+    <>
+      <div>
+        <h2>RH X Management, facilitando sua rotina de departamento pessoal</h2>
+        <p>
+          Precisa de um app para ajudar você no dia-a-dia no RH? Com o RH X Management, você agiliza
+          esse processo
+        </p>
+      </div>
+      <div>
+        <h3>
+          O aplicativo RH X Management ajuda você nas rotinas de departamento pessoal de forma prática e rápida.
+        </h3>
+      </div>
+      <div>
+
+      </div>
+    </>
+  )
+}
+
+export default Content
