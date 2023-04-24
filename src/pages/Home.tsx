6f8af56910faacd56edf5f1fac5ed6a42a0dd301
Marcos9868 <marcos_m_ferreira@outlook.com>
2023-04-24 20:39:03 -0300

    feat(Add): main page of app

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import Content from "../components/content/Content"
+import Footer from "../components/footer/Footer"
+import Header from "../components/header/Header"
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Content />
+      <Footer />
+    </>
+  )
+}
+
+export default Home
