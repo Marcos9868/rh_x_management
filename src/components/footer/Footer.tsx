ccb1ff74b3b80456fc93e19c7175e3d5939ebad2
Marcos9868 <marcos_m_ferreira@outlook.com>
2023-04-24 20:38:09 -0300

    feat(Add): all footer content

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+  return (
+    <>
+      <div>
+        <ul>
+          <li>Discord</li>
+          <li>Linkedin</li>
+          <li>Outlook</li>
+        </ul>
+      </div>
+      <div>
+        <p>Desenvolvido por Marcos M. Ferreira</p>
+        <p>&copy;Todos os direitos reservados</p>
+      </div>
+    </>
+  )
+}
+
+export default Footer
