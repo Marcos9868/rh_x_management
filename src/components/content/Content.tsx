5d6e030ecf38633eaa9ba6fec3c70e4dc68c61f5
Marcos9868 <marcos_m_ferreira@outlook.com>
2023-04-25 16:01:16 -0300

    fix(change): order two banners on home page

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -1,22 +1,29 @@
+import { ContentContainer, MarketingContainer, SecondMarketingContainer } from "./styles"
+import marketingContainerLogo from '../../assets/logo1.jpg';
+import secondMarketingLogo from '../../assets/logo2.jpg'
+
 const Content = () => {
   return (
-    <>
-      <div>
-        <h2>RH X Management, facilitando sua rotina de departamento pessoal</h2>
-        <p>
-          Precisa de um app para ajudar você no dia-a-dia no RH? Com o RH X Management, você agiliza
-          esse processo
-        </p>
-      </div>
-      <div>
-        <h3>
-          O aplicativo RH X Management ajuda você nas rotinas de departamento pessoal de forma prática e rápida.
-        </h3>
-      </div>
-      <div>
-
-      </div>
-    </>
+    <ContentContainer>
+      <MarketingContainer>
+        <img src={marketingContainerLogo} alt="" />
+        <div>
+          <h2>RH X Management, facilitando sua rotina de departamento pessoal</h2>
+          <p>
+            Precisa de um app para ajudar você no dia-a-dia no RH? Com o RH X Management, você agiliza
+            esse processo
+          </p>
+        </div>
+      </MarketingContainer>
+      <SecondMarketingContainer>
+        <div>
+          <h3>
+            O aplicativo RH X Management ajuda você nas rotinas de departamento pessoal de forma prática e rápida.
+          </h3>
+        </div>
+        <img src={secondMarketingLogo} alt="" />
+      </SecondMarketingContainer>
+    </ContentContainer>
   )
 }
 
