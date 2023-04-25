4e455c24661be1d1401ab1f5022dcecc2d782a75
Marcos9868 <marcos_m_ferreira@outlook.com>
2023-04-24 22:04:14 -0300

    fix(remove): background-color of divs and change bg of HeaderContainer

--- src/components/header/styles.ts
+++ src/components/header/styles.ts
@@ -4,22 +4,23 @@ export const HeaderContainer = styled.div`
   display: flex;
   width: 100%;
   height: 5rem;
+  background-color: #181818;
 `;
 
 export const LogoContainer = styled.div`
   display: flex;
   width: 30%;
   height: 5rem;
-  background-color: darkgreen;
   justify-content: center;
   align-items: center;
+  color: #f5f5f5
 `;
 
 export const MenuContainer = styled.div`
   display: flex;
   width: 70%;
   height: 5rem;
-  background-color: darkred;
+  padding-left: 4rem;
   
   ul {
     display: flex;
@@ -29,5 +30,21 @@ export const MenuContainer = styled.div`
     gap: 5rem;
     justify-content: center;
     align-items: center;
+    color: #f5f5f5;
+
+    button {
+      width: 8rem;
+      height: 3rem;
+      background-color: blueviolet;
+      border: none;
+      border-radius: 5px;
+      font-size: medium;
+      cursor: pointer;
+      color: black;
+
+      &:hover {
+        background-color: violet;
+      }
+    }
   }
 `;
