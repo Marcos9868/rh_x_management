84b8ace1a1af63027c485d908890d93c9db1b4d8
Marcos9868 <marcos_m_ferreira@outlook.com>
2023-04-24 21:33:30 -0300

    fix(change): Insert UserImage into FormContainer

--- src/components/login-window/LoginWindow.tsx
+++ src/components/login-window/LoginWindow.tsx
@@ -1,24 +1,34 @@
+import { Button, Form, FormContainer, Input, LoginWindowContainer, UserImage } from "./styles"
+import { AiOutlineUser } from 'react-icons/ai';
+
 const LoginWindow = () => {
   return (
-    <>
-      <h4>Acesso RH X Management</h4>
-      <input 
-        type="email" 
-        name="email" 
-        id="email" 
-        placeholder="Digite seu email" 
-        required
-      />
-      <input 
-        type="password" 
-        name="password" 
-        id="password"
-        placeholder="Digite sua senha"
-        required 
-      />
-      <p>Não possui conta? Criar Conta</p>
-      <button>Entrar</button>
-    </>
+    <LoginWindowContainer>
+      <FormContainer>
+        <UserImage>
+          <AiOutlineUser />
+        </UserImage>
+        <Form>
+          <h4>Acesso RH X Management</h4>
+          <Input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Digite seu email"
+            required
+          />
+          <Input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Digite sua senha"
+            required
+          />
+          <p>Não possui conta? <a href="/sign-up">Criar Conta</a></p>
+          <Button>Entrar</Button>
+        </Form>
+      </FormContainer>
+    </LoginWindowContainer>
   )
 }
 
