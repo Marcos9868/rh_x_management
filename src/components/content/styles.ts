5818263e62f0366fd2739b4005c280dcd8fdd875
Marcos9868 <marcos_m_ferreira@outlook.com>
2023-04-25 16:01:54 -0300

    feat(Add): bg on banners and set display flex

---
+++ src/components/content/styles.ts
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const ContentContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: #222222;
+`;
+
+export const MarketingContainer = styled.div`
+  display: flex;
+  width: 100%;
+  height: 50%;
+  background-color: darkgoldenrod;
+
+  img {
+    width: 70%;
+    height: 50%;
+  }
+`;
+
+export const SecondMarketingContainer = styled.div`
+  display: flex;
+  width: 100%;
+  height: 50%;
+  background-color: antiquewhite;
+
+  img {
+    width: 70%;
+    height: 50%;
+  }
+`;
